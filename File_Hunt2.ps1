693122d3a937051b3c8e62ac2f2700f6240a0aad
JakobraKai <39411724+jahebb@users.noreply.github.com>
2021-09-22 16:07:34 -0400

    Update File_Hunt2.ps1

--- File_Hunt2.ps1
+++ File_Hunt2.ps1
@@ -16,15 +16,18 @@ $break = "======================================================="
 # Move to starting directory
 #Set-Location $env:TEMP 
 
+
 # a. prompts user for a directory to search
 # b. prompts user for a file extension to search
-#$startDir = Read-Host "Please enter starting directory"
-#$fileExt = Read-Host "Please provide a file extension to search (eg .exe, .doc)"
+$startDir = Read-Host "Please enter starting directory (eg C:\Windows\System32)"
+$fileExt = Read-Host "Please provide a file extension to search (eg .exe, .doc, .zip, .txt)"
+Set-Location $startDir
+
 
 ## Debug
-Set-Location 'C:\PowerShell Course'
-$startDir = 'C:\PowerShell Course'
-$fileExt = ".txt"
+#Set-Location 'C:\PowerShell Course'
+#$startDir = 'C:\PowerShell Course'
+#$fileExt = ".txt"
 
 
 $break
@@ -36,7 +39,7 @@ $filenames = gci -Path $startDir -Recurse -Force -ErrorAction SilentlyContinue -
 
 
 # c. return relevant information per file found
-$fileInfo = Get-ItemProperty $filenames
+$fileInfo = $filenames |Select-object Name,Fullname,Directory,Length,lastwritetime,lastaccesstime
 $fileInfo
 Write-Host "Total $fileExt files found: $file_count" -ForegroundColor Green
 Write-Host $break -ForegroundColor Green
@@ -48,45 +51,24 @@ $fileHash
 
 
 # 5. Write relevant data to a file designated by the user
-#$storeInfo = Read-Host "Where would you like to store date? (eg C:\Users\student\Desktop)"
-$storeInfo = 'C:\Users\student\Desktop'
-#$storeName = Read-Host "Name of output file (eg test.txt)"
-$storeName = 'test.txt'
-New-Item -Path $storeInfo -Name $storeName -Force
-$filePath = $storeInfo + "\" + $storeName
+$storeFolder = Read-Host "Where would you like to store results? (eg C:\Users\student\Desktop)"
+#$storeFolder = 'C:\Users\student\Desktop'
+$storeName = Read-Host "Output File Name (eg test)"
+#$storeFile = 'test' + '_' + $(Get-Date -Format yyMMddTHHmm) + '.txt'
+$storeFile = $storeName + '_' + $(Get-Date -Format yyyyMMddTHHmm) + '.txt'
+
+
+New-Item -Path $storeFolder -Name $storeFile -Force
+$filePath = $storeFolder + "\" + $storeFile
 
 Add-Content $filePath "Total $fileExt files found: $file_count"
+Add-Content $filePath $break
 Add-Content $filePath $filenames
+Add-Content $filePath $break
 Add-Content $filePath $fileHash
+Add-Content $filePath $break
 Add-Content $filePath $fileInfo
-
-Set-Location C:\Users\student\Desktop
+Add-Content $filePath $break
 & $filePath
 
-<#
-$break
-
-$stream_count = 0
-$stream_name = New-Object 'System.Collections.Generic.List[System.Object]'
-gci -Path $startDir -Recurse -Force -ErrorAction SilentlyContinue | ForEach-Object { 
-        $dirname = $_.DirectoryName
-        if (Get-Item -Path $_.FullName -Stream *| Where-Object stream -NE ':$DATA' | Format-Table *){
-            Write-Host "Filename is $_" -ForegroundColor Cyan
-            $sha1 = (Get-FileHash $_.FullName -Algorithm SHA1).Hash
-            $last_4_sha1 = $sha1.substring($sha1.length - 4, 4)
-            Write-Host "Last 4 of SHA1 of $_ is  $last_4_sha1 " -ForegroundColor Cyan
-            $stream_name = (Get-Item -Path $_.FullName -Stream *| Where-Object stream -NE ':$DATA')| Select-Object Stream
-            $ads_name = ($stream_name).stream
-            Get-Content -Path $_.FullName -Stream ($stream_name).Stream | Out-File $ads_name
-            Write-Host "Writing ADS to files $ads_name" -ForegroundColor Cyan
-            Write-Host "File $ads_name in $dirname " -ForegroundColor Cyan
-           
-            
-            Write-Host ""
-            $stream_count++
-        }
-        
-    }
-Write-Host "Total files count is: $stream_count" -ForegroundColor Green
-Write-Host $break -ForegroundColor Green
-#>
+Set-Location $storeFolder
