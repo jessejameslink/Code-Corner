434c2bef34dd0b10a0768a9a5daa90bb4b69aa9f
JakobraKai <39411724+jahebb@users.noreply.github.com>
2021-09-22 14:24:01 -0400

    Update File_Hunt2.ps1

--- File_Hunt2.ps1
+++ File_Hunt2.ps1
@@ -14,35 +14,43 @@ $break = "======================================================="
 
 
 # Move to starting directory
-cd $env:TEMP 
+#Set-Location $env:TEMP 
 
 # a. prompts user for a directory to search
-$startDir = Read-Host "Please enter starting directory"; 
-$fileExt = Read-Host "Please provide a file extension to search (eg .exe, .doc)"
+# b. prompts user for a file extension to search
+#$startDir = Read-Host "Please enter starting directory"
+#$fileExt = Read-Host "Please provide a file extension to search (eg .exe, .doc)"
+
+## Debug
+Set-Location 'C:\PowerShell Course'
+$startDir = 'C:\PowerShell Course'
+$fileExt = ".txt"
+
 
 $break
 
 $file_count = 0
 gci -Path $startDir -Recurse -Force -ErrorAction SilentlyContinue -Include *$fileExt | ForEach-Object { $file_count++ }
-Write-Host "Total {$fileExt} is: $file_count" -ForegroundColor Green
+$filenames = gci -Path $startDir -Recurse -Force -ErrorAction SilentlyContinue -Include *$fileExt
+$files = @($filenames | foreach {$_.Name} | Format-Table name)
+Write-Host "Total $fileExt is: $file_count" -ForegroundColor Green
 Write-Host $break -ForegroundColor Green
+
+# c. return relevant information per file found
 
 $break
 
 $stream_count = 0
 $stream_name = New-Object 'System.Collections.Generic.List[System.Object]'
-gci -Path $starDir -Recurse -Force -ErrorAction SilentlyContinue | 
-    ForEach-Object { 
+gci -Path $startDir -Recurse -Force -ErrorAction SilentlyContinue | ForEach-Object { 
         $dirname = $_.DirectoryName
-        if (Get-Item -Path $_.FullName -Stream *| Where-Object stream -NE ':$DATA' | Format-Table *) {
+        if (Get-Item -Path $_.FullName -Stream *| Where-Object stream -NE ':$DATA' | Format-Table *){
             Write-Host "Filename is $_" -ForegroundColor Cyan
             $sha1 = (Get-FileHash $_.FullName -Algorithm SHA1).Hash
             $last_4_sha1 = $sha1.substring($sha1.length - 4, 4)
-            
             Write-Host "Last 4 of SHA1 of $_ is  $last_4_sha1 " -ForegroundColor Cyan
-            
             $stream_name = (Get-Item -Path $_.FullName -Stream *| Where-Object stream -NE ':$DATA')| Select-Object Stream
-            $ads_name = ($stream_name).Stream
+            $ads_name = ($stream_name).stream
             Get-Content -Path $_.FullName -Stream ($stream_name).Stream | Out-File $ads_name
             Write-Host "Writing ADS to files $ads_name" -ForegroundColor Cyan
             Write-Host "File $ads_name in $dirname " -ForegroundColor Cyan
