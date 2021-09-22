fcaaad030d02a9098e4985ef7fe80ba55810b6f5
JakobraKai <39411724+jahebb@users.noreply.github.com>
2021-09-22 12:48:43 -0400

    Update Chaos1.ps1

--- Chaos1.ps1
+++ Chaos1.ps1
@@ -1,11 +1,15 @@
-Set-Content .\PWND.ps1 -Value "$val=0 
-while($val -ne 2)
-{ 
-Set-Location C:\Users\student\Desktop
-New-Item .\PWND.txt
-Set-Content .\PWND.txt 'YOU HAVE BEEN PWND!!!  PAY ME ALL YOUR WAMPUM AND I WILL SET YOU FREE!!!'
-.\PWND.txt
-$val++
-Write-Host $val
-Start-Sleep -s 15
-}"
+$code = {
+  $val=0;
+  while($val -ne 6) {
+  Set-Location C:\Users\student\Desktop;
+  New-Item .\PWND.txt -ErrorAction SilentlyContinue;
+  Set-Content .\PWND.txt 'YOU HAVE BEEN PWND!!!  PAY ME ALL YOUR WAMPUM AND I WILL SET YOU FREE!!!';
+  .\PWND.txt;
+  Invoke-WmiMethod -Class Win32_Process -Name Create -ArgumentList "C:\Windows\System32\msg.exe * YOU HAVE BEEN PWND!!! PAY ME ALL YOUR WAMPUM AND I WILL SET YOU FREE!!!";
+  $val++;
+  Write-Host $val;
+  Start-Sleep -s 15
+  }
+}
+
+Set-Content .\PWND.ps1 -Value $code
