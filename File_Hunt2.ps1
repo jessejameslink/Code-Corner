d09187a80a9beaa3674be2f308404225cd10056d
JakobraKai <39411724+jahebb@users.noreply.github.com>
2021-09-23 10:55:13 -0400

    Update File_Hunt2.ps1

--- File_Hunt2.ps1
+++ File_Hunt2.ps1
@@ -1,74 +1,76 @@
-# Write a script that:
-# a. prompts user for a directory to search
-# b. prompts user for a file extension to search
-# c. return relevant information per file found
-# d. add flow control
-# 1. Look for another file ext.
-# 2. Another Directory
-# 3. Handle a list/array of #1 & #2
-# 4. Get-FileHash
-# 5. Write relevant data to a file designated by the user.
+
+<#
+Write a script that:
+Prompts user for a directory to search
+Prompts user for a file extension to search
+Return relevant information per file found
+Add flow control
+Look for another file ext.
+Another Directory
+Handle a list/array of #1 & #2
+Get-FileHash
+Write relevant data to a file designated by the user.
+#>
+
+# Move to starting directory (Optional)
+# Set-Location $env:TEMP 
 
 # Environment Variables
-$break = "======================================================="
+$break = "==============================================================================================="
+$flag = $true
 
 
-# Move to starting directory
-#Set-Location $env:TEMP 
+while ($flag -eq $true)
+{
+    # Write relevant data to a file designated by the user
+    $storeFolder = Read-Host "Where would you like to store results? (eg C:\Users\student\Desktop)"
+    $storeName = Read-Host "Output File Name (eg test)"
+    #$storeFile = 'test' + '_' + $(Get-Date -Format yyMMddTHHmm) + '.txt'
+    $storeFile = $storeName + '_' + $(Get-Date -Format yyyyMMddTHHmm) + '.txt'
+    New-Item -Path $storeFolder -Name $storeFile -Force
+    $filePath = $storeFolder + "\" + $storeFile
 
 
-# a. prompts user for a directory to search
-# b. prompts user for a file extension to search
-$startDir = Read-Host "Please enter starting directory (eg C:\Windows\System32)"
-$fileExt = Read-Host "Please provide a file extension to search (eg .exe, .doc, .zip, .txt)"
-Set-Location $startDir
+    # Prompts user for a directory to search
+    $dirList = @()
+    do {
+        $searchDir = (Read-Host "Please enter search directory (eg C:\Windows\System32)")
+            if($searchDir -ne ''){$dirList += $searchDir}
+        $inputMod = Read-Host "Would you like to search additional directories?(y/n)"
+        }
+    until($inputMod -eq 'n')
 
 
-## Debug
-#Set-Location 'C:\PowerShell Course'
-#$startDir = 'C:\PowerShell Course'
-#$fileExt = ".txt"
+    # Prompts user for a file extension to search
+    $extList = @()
+    do {
+        $fileExt = (Read-Host "Please provide a file extension to search (eg .exe, .doc, .zip, .txt)")
+            if($fileExt -ne ''){$extList += $fileExt}
+        $inputMod = Read-Host "Would you like to search additional extensions?(y/n)"
+        }
+    until($inputMod -eq 'n')
+
+    # Return relevant information per file found
+    # Return File Hash per file found
+    $file_count = 0
+    ForEach ($dir in $dirList){
+        ForEach ($ext in $extList){
+            $Hash = (Get-Childitem -Path $dir\*$ext|get-filehash)
+            $Data = (Get-Childitem -path $dir\*$ext|Select-object Name,Fullname,Directory,Length,lastwritetime,lastaccesstime)
+            $Hash | Out-File -FilePath $filePath -Append
+            $break | Out-File -FilePath $filePath -Append
+            $Data | Out-File -FilePath $filePath -Append
+            $break | Out-File -FilePath $filePath -Append}}
 
 
-$break
+    # Navigate to File Location Where File is Stored
+    Set-Location $storeFolder
+    & $filePath
+    
 
-
-$file_count = 0
-Get-ChildItem -Path $startDir -Recurse -Force -ErrorAction SilentlyContinue | Where-Object {$_.Extension -eq $fileExt} | ForEach-Object { $file_count++; Write-Host $_.FullName } 
-$filenames = gci -Path $startDir -Recurse -Force -ErrorAction SilentlyContinue -Include *$fileExt
-
-
-# c. return relevant information per file found
-$fileInfo = $filenames |Select-object Name,Fullname,Directory,Length,lastwritetime,lastaccesstime
-$fileInfo
-Write-Host "Total $fileExt files found: $file_count" -ForegroundColor Green
-Write-Host $break -ForegroundColor Green
-
-
-# 4. Get-FileHash
-$fileHash = Get-FileHash -Algorithm MD5 $filenames
-$fileHash
-
-
-# 5. Write relevant data to a file designated by the user
-$storeFolder = Read-Host "Where would you like to store results? (eg C:\Users\student\Desktop)"
-#$storeFolder = 'C:\Users\student\Desktop'
-$storeName = Read-Host "Output File Name (eg test)"
-#$storeFile = 'test' + '_' + $(Get-Date -Format yyMMddTHHmm) + '.txt'
-$storeFile = $storeName + '_' + $(Get-Date -Format yyyyMMddTHHmm) + '.txt'
-
-
-New-Item -Path $storeFolder -Name $storeFile -Force
-$filePath = $storeFolder + "\" + $storeFile
-
-Add-Content $filePath "Total $fileExt files found: $file_count"
-Add-Content $filePath $break
-Add-Content $filePath $filenames
-Add-Content $filePath $break
-Add-Content $filePath $fileHash
-Add-Content $filePath $break
-Add-Content $filePath $fileInfo
-Add-Content $filePath $break
-& $filePath
-
-Set-Location $storeFolder
+    $response = Read-Host "Would you like to START a new search?(y/n)"
+    if ($response -eq "y")
+        {$flag = $true}
+    Else 
+        {$flag = $false}
+}
