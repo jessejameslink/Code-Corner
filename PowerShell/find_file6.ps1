69d42580ec3d88a3487b7625a2ce691ef6fc694b
JakobraKai <39411724+jahebb@users.noreply.github.com>
2021-09-23 20:21:29 -0400

    Update find_file6.ps1
    hardcoded DLL search, and can search for additional extensions

--- PowerShell/find_file6.ps1
+++ PowerShell/find_file6.ps1
@@ -1,5 +1,79 @@
-############################### HELP SECTION ###############################
-​
+
+
+
+cd $env:COMPUTERNAME
+cls
+
+
+Function FileFinder
+    {
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory=$true)]
+        [array] $dirList,
+        [Parameter(Mandatory=$true)]
+        [array] $extList
+    )
+
+    # Include dll file extension
+    $extList += 'dll'
+
+
+    # Create a list of discovered files
+    $resultsList= @()
+    ForEach ($dir in $dirList)
+    {
+        ForEach ($ext in $extList)
+        {
+            $resultsList += Get-ChildItem -Path $dir "*.$ext" -ErrorAction SilentlyContinue
+        }
+    }
+    ForEach ($result in $resultsList)
+    {
+        $hash = Get-FileHash -Algorithm MD5 -Path $result.FullName
+        $result | Add-Member -MemberType NoteProperty -Name MD5 -Value $hash.hash 
+    }
+    ForEach ($result in $resultsList)
+    {
+        $signature = Get-AuthenticodeSignature -FilePath $result.Fullname 
+        if ($signature.status -eq "Valid")
+        {
+            $result | Add-Member -MemberType NoteProperty -Name SigSubject -Value $signature.SignerCertificate.Subject
+            $result | Add-Member -MemberType NoteProperty -Name SigThumbPrint -Value $signature.SignerCertificate.Thumbprint
+        }
+    }
+
+
+    # Set Out-File Settings
+    $homeFolder = "~\Documents"
+    $validFile = "valid_sig" + '_' + $(Get-Date -Format yyyyMMdd) + '.log'
+    $invalidFile = "invalid_sig" + '_' + $(Get-Date -Format yyyyMMdd) + '.log'
+    $validfilePath = $homeFolder + "\" + $validFile
+    $invalidfilePath = $homeFolder + "\" + $invalidFile
+    New-Item -Path $homeFolder -Name $validFile -Force
+    New-Item -Path $homeFolder -Name $invalidFile -Force
+
+
+    #File hash, creation date, last accessed, & location
+    #File hash, creation date, signing org, & location
+    $resultsList | Where-Object {$_.SigSubject -ne $NULL} | Select-object -Property Fullname,MD5,SigSubject,SigThumbPrint,creationtime | Out-File -FilePath $validfilePath -Force -Append
+    
+
+    # If invalid, log to invalid_sigs.log, & print to stdout
+    # File hash, creation date, last accessed, & location
+    $resultsList | Where-Object {$_.SigThumbPrint -eq $NULL} | Select-object -Property Fullname,MD5,creationtime,lastaccesstime | Out-File -FilePath $invalidfilePath -Force -Append
+    $invalid = $resultsList | Where-Object {$_.SigThumbPrint -eq $NULL} | Select-object -Property Fullname,MD5,creationtime,lastaccesstime
+    $invalid | Where-Object {$_.SigThumbPrint -eq $NULL} |Write-Host "$invalid"
+    
+
+    # Navigate to File Location Where File is Stored and Open
+    Set-Location $homeFolder
+    & $validfilePath
+    & $invalidfilePath
+    }
+
+############################### HELP SECTION ###############################​
 <#
     .SYNOPSIS
     Validate signatures in signed executables, dynamic link libraries, drivers, and other files supporting signatures.
@@ -29,75 +103,5 @@
     .LINK
     Get-FileHash
 #>
-
-
-cd $env:COMPUTERNAME
-cls
-
-
-Function FileFinder
-    {
-    [CmdletBinding()]
-    param
-    (
-        [Parameter(Mandatory=$true)]
-        [array] $dirList,
-        [Parameter(Mandatory=$false)]
-        [array] $extList = @("dll")
-    )
-
-    # Create a list of discovered files
-    $resultsList= @()
-    ForEach ($dir in $dirList)
-    {
-        ForEach ($ext in $extList)
-        {
-            $resultsList += Get-ChildItem -Path $dir "*.$ext" -ErrorAction SilentlyContinue
-        }
-    }
-    ForEach ($result in $resultsList)
-    {
-        $hash = Get-FileHash -Algorithm MD5 -Path $result.FullName
-        $result | Add-Member -MemberType NoteProperty -Name MD5 -Value $hash.hash 
-    }
-    ForEach ($result in $resultsList)
-    {
-        $signature = Get-AuthenticodeSignature -FilePath $result.Fullname 
-        if ($signature.status -eq "Valid")
-        {
-            $result | Add-Member -MemberType NoteProperty -Name SigSubject -Value $signature.SignerCertificate.Subject
-            $result | Add-Member -MemberType NoteProperty -Name SigThumbPrint -Value $signature.SignerCertificate.Thumbprint
-        }
-    }
-
-
-    # Set Out-File Settings
-    $homeFolder = "~\Desktop"
-    $validFile = "valid_sig" + '_' + $(Get-Date -Format yyyyMMdd) + '.log'
-    $invalidFile = "invalid_sig" + '_' + $(Get-Date -Format yyyyMMdd) + '.log'
-    $validfilePath = $homeFolder + "\" + $validFile
-    $invalidfilePath = $homeFolder + "\" + $invalidFile
-    New-Item -Path $homeFolder -Name $validFile -Force
-    New-Item -Path $homeFolder -Name $invalidFile -Force
-
-
-    #File hash, creation date, last accessed, & location
-    #File hash, creation date, signing org, & location
-    $resultsList | Where-Object {$_.SigSubject -ne $NULL} | Select-object -Property Fullname,MD5,SigSubject,SigThumbPrint,creationtime | Out-File -FilePath $validfilePath -Force -Append
-    
-
-    # If invalid, log to invalid_sigs.log, & print to stdout
-    # File hash, creation date, last accessed, & location
-    $resultsList | Where-Object {$_.SigThumbPrint -eq $NULL} | Select-object -Property Fullname,MD5,creationtime,lastaccesstime | Out-File -FilePath $invalidfilePath -Force -Append
-    $invalid = $resultsList | Where-Object {$_.SigThumbPrint -eq $NULL} | Select-object -Property Fullname,MD5,creationtime,lastaccesstime
-    $invalid | Where-Object {$_.SigThumbPrint -eq $NULL} |Write-Host "$invalid"
-    
-
-    # Navigate to File Location Where File is Stored and Open
-    Set-Location $homeFolder
-    & $validfilePath
-    & $invalidfilePath
-    }
-
 
 FileFinder
